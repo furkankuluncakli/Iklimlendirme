32811bf5afdfd6266bc1912a722799b80ab326e5
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-03-22 23:19:31 +0300

    feat: add services data and dynamic rendering to projects page

--- iklimlendirme/src/app/projects/page.js
+++ iklimlendirme/src/app/projects/page.js
@@ -1,26 +1,28 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 const Projects = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("/services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data))
+      .catch((err) => console.error("Veri Çekme Hatası :", err));
+  }, []);
+
   return (
     <>
       <div className={styles.container}>
         <div className={styles.cardContainer}>
-          <div className={styles.card}>
-            <img src="img/logo-19.png" alt="Foto" />
-          </div>
-          <div className={styles.card}>
-            <img src="img/logo.jpg" alt="Foto" />
-          </div>
-          <div className={styles.card}>
-            <img src="img/havalandirma.jpg" alt="Foto" />
-          </div>
-          <div className={styles.card}>
-            <img src="img/logo.jpg" alt="Foto" />
-          </div>
-          <div className={styles.card}>
-            <img src="img/logo.jpg" alt="Foto" />
-          </div>
+          {services.map((service) => (
+            <div key={service.id} className={styles.card}>
+              <img src={service.img} alt={service.title} />
+              <h3>{service.title}</h3>
+              <p>{service.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </>
