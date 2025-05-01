f997c4cfc17ad016fa0004a4e509a8d9b1f9ec7d
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-05-01 18:25:10 +0300

    fix: pagination visibility and adjust slider layout

--- iklimlendirme/src/components/References.js
+++ iklimlendirme/src/components/References.js
@@ -1,14 +1,25 @@
-import React, { useRef, useState } from "react";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import styles from "./References.module.css";
 
-import { Pagination, Autoplay } from "swiper/modules"; // Import Autoplay module
+import { Pagination, Autoplay } from "swiper/modules";
 import Image from "next/image";
 
 export default function App() {
+  const images = [
+    { img: "/img/logo.jpg", alt: "Image 1" },
+    { img: "/img/logo.jpg", alt: "Image 2" },
+    { img: "/img/logo.jpg", alt: "Image 3" },
+    { img: "/img/logo.jpg", alt: "Image 4" },
+    { img: "/img/logo.jpg", alt: "Image 5" },
+    { img: "/img/logo.jpg", alt: "Image 6" },
+    { img: "/img/logo.jpg", alt: "Image 7" },
+    { img: "/img/logo.jpg", alt: "Image 8" },
+    { img: "/img/logo.jpg", alt: "Image 9" },
+  ];
+
   return (
     <>
       <Swiper
@@ -19,38 +30,26 @@ export default function App() {
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination, Autoplay]} // Add Autoplay module here
-        className="mySwiper"
+        modules={[Pagination, Autoplay]}
+        className={styles.slider} 
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
         }}
         loop={true}
       >
-        <SwiperSlide>
-          <Image src={`/img/logo.jpg`} width={250} height={250} alt="logo" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={`/img/logo.jpg`} width={250} height={250} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={`/img/logo.jpg`} width={250} height={250} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={`/img/logo.jpg`} width={250} height={250} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={`/img/logo.jpg`} width={250} height={250} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={`/img/logo.jpg`} width={250} height={250} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={`/img/logo.jpg`} width={250} height={250} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={`/img/logo.jpg`} width={250} height={250} />
-        </SwiperSlide>
+        {images.map((image, index) => (
+          <SwiperSlide key={index} className={styles.sliderSlide}>
+            <div className={styles.imageContainer}>
+              <Image
+                src={image.img}
+                alt={image.alt}
+                layout="fill"
+                className={styles.image}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
