dd066e32240b44119c2a46d4af50b7ae0b2104ca
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-05-01 14:59:05 +0300

    fix: layout shift in Statistic Seciton

--- iklimlendirme/src/components/StatisticsSection.js
+++ iklimlendirme/src/components/StatisticsSection.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import CountUp from 'react-countup';
-import { FaBriefcase, FaProjectDiagram, FaSmile } from 'react-icons/fa';
-import styles from './StatisticsSection.module.css'; 
+import React from "react";
+import CountUp from "react-countup";
+import { FaBriefcase, FaProjectDiagram, FaSmile } from "react-icons/fa";
+import styles from "./StatisticsSection.module.css";
 
 const StatisticsSection = () => {
   return (
     <section className={styles.statsSection}>
       <div className={styles.statBox}>
         <FaBriefcase className={styles.icon} />
-        <span className={styles.statNumber}>
-          <CountUp end={20} duration={2} suffix="+" />
+        <span
+          className={styles.statNumber}
+          style={{ minWidth: "4ch", textAlign: "center" }}
+        >
+          <CountUp start={1} end={20} duration={2} suffix="+" />
         </span>
         <span className={styles.statLabel}>
           Yıllık <br /> Deneyim
@@ -17,8 +20,11 @@ const StatisticsSection = () => {
       </div>
       <div className={styles.statBox}>
         <FaProjectDiagram className={styles.icon} />
-        <span className={styles.statNumber}>
-          <CountUp end={1000} duration={2.5} suffix="+" />
+        <span
+          className={styles.statNumber}
+          style={{ minWidth: "6ch", textAlign: "center" }}
+        >
+          <CountUp start={100} end={1000} duration={2.5} suffix="+" />
         </span>
         <span className={styles.statLabel}>
           Tamamlanan <br /> Proje
@@ -26,8 +32,11 @@ const StatisticsSection = () => {
       </div>
       <div className={styles.statBox}>
         <FaSmile className={styles.icon} />
-        <span className={styles.statNumber}>
-          <CountUp end={100} duration={2} suffix="%" />
+        <span
+          className={styles.statNumber}
+          style={{ minWidth: "4ch", textAlign: "center" }}
+        >
+          <CountUp start={50} end={100} duration={2} suffix="%" />
         </span>
         <span className={styles.statLabel}>
           Müşteri <br /> Memnuniyeti
